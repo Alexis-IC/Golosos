2be73c29c8910f40f49b913491b9d0c3adbc4b33
Alexis Aldana <on0xs@hotmail.com>
2020-07-21 14:49:31 -0600

    View contact and about

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +8,31 @@ namespace LosGolosos.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        
     }
 }
